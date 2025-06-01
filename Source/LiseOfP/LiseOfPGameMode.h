73fb3f0f73770d8c71b350bd8e951d5119d1be43
HaloTwo <lsm6265@naver.com>
2025-06-01 23:56:17 +0900

    Initial commit

---
+++ Source/LiseOfP/LiseOfPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LiseOfPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALiseOfPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALiseOfPGameMode();
+};
+
+
+
